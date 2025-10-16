f04ab188be51adef3db341d6041056a23670eda4
sania parvej <sparvej@rrc.ca>
2025-10-15 22:49:26 -0500

    Add PaymentStrategy abstract class

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,13 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Sania Parvej"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+from payee.payee import Payee
+from billing_account.billing_account import BillingAccount
+
+class PaymentStrategy(ABC):
+    @abstractmethod
+    def process_payment(self, account: BillingAccount, payee: Payee, amount: float) -> str:
+        pass
