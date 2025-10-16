97ad4ed6428f8c534ebce3c6ce226ac0efa44664
sania parvej <sparvej@rrc.ca>
2025-10-16 00:38:55 -0500

    Add Payment class implementing Strategy Pattern.

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,21 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Sania Parvej"
+__version__ = "1.0.0"
 
-from billing_account.billing_account import BillingAccount
+from billing_account.billing_account import PaymentStrategy
 from payee.payee import Payee
+from billing_account.billing_account import BillingAccount
+
+class Payment:
+    def __init__(self, strategy: PaymentStrategy):
+        #If the value is of PaymentStrategy
+        if isinstance(strategy, PaymentStrategy):
+            self.__strategy = strategy
+        #If the value is is not of PaymentStrategy, raise a ValueError
+        else:
+            raise ValueError("Invalid Strategy")
+        
+    def pay_bill(self, account: BillingAccount, payee: Payee, amount: float) -> str:
+       
+        return self.__strategy.process_payment(account, payee, amount)
